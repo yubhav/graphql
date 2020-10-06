ac7412586e0ba8cb0943a4bdd11808f47e08a124
Yubhav Seth <yubhav.seth97@gmail.com>
2020-10-06 11:26:01 +0530

    db setup

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql').graphqlHTTP;
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -39,22 +40,53 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+              // OR { ...event._doc, _id: event._doc._id.toString() }
+              // event.id is an easy way offered to us by mongoose
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
       createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
       }
     },
     graphiql: true
   })
 );
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+        process.env.MONGO_PASSWORD
+      }@cluster0.4wcip.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+      {useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true}
+  )
+  .then(() => {
+    app.listen(3000, console.log("Your server is up dude!"));
+  })
+  .catch(err => {
+    console.log(err);
+  });
